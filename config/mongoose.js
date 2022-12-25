f0f6f225178e83a817fe486788fd1cf3610c3673
Md Umar <mohammadumar389@gmail.com>
2022-12-25 16:31:43 +0530

    final setup done

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose=require('mongoose');
-//mongodb+srv://umar389:test786@cluster0.dtbfvbd.mongodb.net/clg_result ${process.env.MONGODB_URI}
 mongoose.connect(process.env.MONGOURI);
 
 const db=mongoose.connection;
